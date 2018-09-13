877267525e9bc0c503a469c0a0cc7ff145d33060
chong050 <chong050@morris.umn.edu>
2018-09-13 14:29:04 -0500

    Updated README - includes all trials. Added comments to main explaining how to run the program.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Main {
 
+    // Quicksort method based on psudocode from book
     public static void quicksort(TestInteger[] quickArray, int start, int end){
         if (start < end){
             int q = partition(quickArray, start, end);
@@ -10,6 +11,7 @@ public class Main {
         }
     }
 
+    // Partitions array into smaller parts for quicksort based on psudocode from book
     public static int partition(TestInteger[] partArray, int start, int end){
         TestInteger x = partArray[end];
         int i = start - 1;
@@ -26,6 +28,7 @@ public class Main {
         return i+1;
     }
 
+    // Returns if an array is sorted or not
     public static boolean isSorted(TestInteger[] boolArray){
         for (int i = 1; i < boolArray.length; i++){
             if (boolArray[i-1].compareTo(boolArray[i]) == 1){
@@ -35,19 +38,48 @@ public class Main {
         return true;
     }
 
+    // Exchange - swap two elements in an array
     public static void exchange(int arrayPositionOne, int arrayPositionTwo, TestInteger[] Array){
         TestInteger temp = Array[arrayPositionOne];
         Array[arrayPositionOne] = Array[arrayPositionTwo];
         Array[arrayPositionTwo] = temp;
     }
 
+    // Array generation and info output
     public static void main(String[] args) {
         // Create array - Generate 10,000 random TestIntegers in range 1-1,000,000 using Math.random
         TestInteger[] timArray = new TestInteger[10000];
 
-        // Populate array
-        for (int i = 0; i < 10000; i++){
+        // Used in last two array generators
+        int counter = 0;
+
+        // Populate array - Change i to modify number of sorted sequences
+        // Make sure only one section is active at a time!
+        for (int i = 0; i < 100; i++){
+            // Random sequence - default
             timArray[i] = new TestInteger((int)(Math.random()*100000));
+
+            // Increasing order
+            // timArray[i] = new TestInteger((i));
+
+            // Initial value used for starting sequence - only used for following two array generators
+            int initialValue = (int) (Math.random() * 1000000);
+
+            // 10 sorted sequences of 1,000 elements - change c to modify number of elements
+            /*
+            for(int k = initialValue; k < initialValue + 1000; k++){
+                timArray[counter] = new TestInteger(k);
+                counter++;
+            }
+            */
+
+            // 100 sorted sequences of 100 elements - change c to modify number of elements
+            /*
+            for(int k = initialValue; k < initialValue + 100; k++){
+                timArray[counter] = new TestInteger(k);
+                counter++;
+            }
+            */
         }
 
         // Clone array using .clone
